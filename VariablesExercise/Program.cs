fa9d86174eb375f444cd4bbd1bbb01fbc17b7acc
Akdutt47 <ankurit145@gmail.com>
2024-01-17 10:40:46 -0800

    Finished

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Declareing & Initialize variables
+            string name = "Bob";
+            int age = 31;
+            char middleInitial = 'R';
+            bool isOver18 = true;
+            double currentTemp = 88.3;
+            decimal currentPrice = 22.43m;
+
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Age: {age}");
+            Console.WriteLine($"Middle Initial: {middleInitial}");
+            Console.WriteLine($"Is Over 18: {isOver18}");
+            Console.WriteLine($"Current Temp: {currentTemp}");
+            Console.WriteLine($"Current Price: {currentPrice}");
         }
     }
 }
